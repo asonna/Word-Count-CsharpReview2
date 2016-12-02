53bdba982961ada4b6252740cd68f59b8362e811
Annie Sonna <nsab42@yahoo.com>
2016-12-02 10:15:42 -0800

    initial commit

---
+++ Objects/WordCount.cs
@@ -0,0 +1,11 @@
+using System.Collections.Generic;
+using System;
+using System.Linq;
+
+namespace TheWordCount.Objects
+{
+	public class WordCount
+	{
+		
+	}
+}
---
+++ Tests/WordCountTest.cs
@@ -0,0 +1,12 @@
+using Xunit;
+using System.Collections.Generic;
+using System;
+
+namespace TheWordCount.Objects
+{
+  public class WordCountTest
+  {
+    [Fact]
+    
+  }
+}
