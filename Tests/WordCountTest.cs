89a1eeb887900a7c1699f30815427dd2f0f0279d
Annie Sonna <nsab42@yahoo.com>
2016-12-02 12:37:51 -0800

    Just failed the 2nd behavior test completed on both behaviors 1 & 2

--- Tests/WordCountTest.cs
+++ Tests/WordCountTest.cs
@@ -13,6 +13,11 @@ namespace TheWordCount.Objects
       Assert.Equal(1, newWord.CountRepeats("WORD", "WORD"));
     }
 
-
+    [Fact]
+    public void CountRepeats_ForSingleWordAgainstStringOfWordIncludingItselfOnce_1()
+    {
+      RepeatCounter newWord = new RepeatCounter("LIFE IS NOT EASY", "LIFE");
+      Assert.Equal(1, newWord.CountRepeats("LIFE IS NOT EASY", "LIFE"));
+    }
   }
 }
