394e48cd5c2e51b0ce0fa599c6b6c5bb4c3a95e1
Annie Sonna <nsab42@yahoo.com>
2016-12-02 14:33:41 -0800

    Test passing for the current three behaviors

--- Tests/WordCountTest.cs
+++ Tests/WordCountTest.cs
@@ -15,10 +15,20 @@ namespace TheWordCount.Objects
     }
 
     [Fact]
-    public void CountRepeats_ForSingleWordAgainstStringOfWordIncludingItselfOnce_1()
+    public void CountRepeats_ForSingleWordAgainstStringOfWordWithSingleMatch_1()
     {
       RepeatCounter newWord = new RepeatCounter("LIFE", "LIFE IS NOT EASY");
       Assert.Equal(1, newWord.CountRepeats("LIFE", "LIFE IS NOT EASY"));
     }
+
+    [Fact]
+    public void CountRepeats_ForSingleWordAgainstStringOfWordWithMultipleMatch_3()
+    {
+      RepeatCounter newWord = new RepeatCounter("DOG", "A DOG OWNER TOOK HIS DOG INTO A DOG STORE");
+      Assert.Equal(3, newWord.CountRepeats("DOG", "A DOG OWNER TOOK HIS DOG INTO A DOG STORE"));
+    }
+
+
+
   }
 }
